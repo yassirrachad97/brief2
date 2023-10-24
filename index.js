bd2402023bfef1a70455da4d4a65685ff0b0411e
yassir rachad <yassirrachad1997@gmail.com>
2023-10-24 23:28:23 +0100

    avant validation form

--- index.js
+++ index.js
@@ -1,38 +1,53 @@
 var btn_menu=document.getElementById("buttonMenu");
 var menu=document.getElementById("composants-menu");
+var qteProduct = document.querySelectorAll(".nombreProduit");
+ var price = document.querySelectorAll(".carats p");
+ var totalprice = document.querySelectorAll(".total_prix_prod");
+ var totalgeneral = document.getElementById("total_general");
 btn_menu.addEventListener("click",()=>{
     if(menu.style.display==="block") menu.style.display="none";
     else if(menu.style.display==="none") menu.style.display="block";
 });
-var qteProduct = document.querySelectorAll(".nombreProduit");
-    var price = document.querySelectorAll(".carats p");
-    var totalprice = document.querySelectorAll(".total_prix_prod");
-    var totalgeneral = document.getElementById("total_general");
 
     function calculerTotalProduit(quantite, prix) {
         return quantite * prix;
     }
-
-    function calculerTotalGeneral() {
-        var total = 0;
-        for (var i = 0; i < qteProduct.length; i++) {
-            var quantite = parseInt(qteProduct[i].value);
-            var prix = parseFloat(price[i].textContent);
-            var totalProduit = calculerTotalProduit(quantite, prix);
-            totalprice[i].textContent = totalProduit;
-            total += totalProduit;
-        }
-        totalgeneral.textContent = total;
-    }
-
-    calculerTotalGeneral();
 
     for (var i = 0; i < qteProduct.length; i++) {
         qteProduct[i].addEventListener("input", function (event) {
             calculerTotalGeneral();
         });
     }
+    let produit=document.getElementsByClassName("produit");
     delet1.addEventListener("click",()=>{
-        document.getElementById("pro1").style.display="none";
+       produit[0].style.display="none";
+       calculerTotalGeneral();
     })
+    delet2.addEventListener("click",()=>{
+        produit[1].style.display="none";
+        calculerTotalGeneral();
+     })
+     delet3.addEventListener("click",()=>{
+        produit[2].style.display="none";
+        calculerTotalGeneral(); 
+     })
+
+     function calculerTotalGeneral() {
+        var total = 0;
+        for (var i = 0; i < qteProduct.length; i++) {
+            if(produit[i].style.display!="none"){
+            var quantite = parseInt(qteProduct[i].value);
+            var prix = parseFloat(price[i].innerHTML);
+            var totalProduit = calculerTotalProduit(quantite, prix);
+            totalprice[i].innerHTML = totalProduit;
+            total += totalProduit;
+
+            }
+            
+        }
+        totalgeneral.innerHTML = total+".00$";
+    }
+   
+function validationEmail(email)
+{}
 
